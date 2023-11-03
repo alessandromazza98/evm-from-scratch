a7811fcfbd7f404598315f86f8199118eda30131
alessandromazza <alessandro98.mazza@gmail.com>
2023-11-03 16:35:07 +0100

    add opcodes! macro

--- rust/src/opcode.rs
+++ rust/src/opcode.rs
@@ -1,26 +1,30 @@
-pub enum OpCode {
-    Stop = 0,
-    Push0 = 95,
-    Push1 = 96,
-    Push2 = 97,
-    Push4 = 99,
-    Push6 = 101,
+macro_rules! opcodes {
+    ($($name:ident($number:expr),)*) => {
+        #[derive(Debug)]
+        pub enum OpCode {
+            $($name = $number,)*
+        }
+
+        impl TryFrom<u8> for OpCode {
+            type Error = String;
+
+            fn try_from(value: u8) -> Result<Self, Self::Error> {
+                match value {
+                    $($number => Ok(OpCode::$name),)*
+                    _ => Err("Unknown opcode".to_string()),
+                }
+            }
+        }
+    };
 }
 
-impl TryFrom<u8> for OpCode {
-    type Error = String;
-
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
-        match value {
-            0 => Ok(OpCode::Stop),
-            95 => Ok(OpCode::Push0),
-            96 => Ok(OpCode::Push1),
-            97 => Ok(OpCode::Push2),
-            99 => Ok(OpCode::Push4),
-            101 => Ok(OpCode::Push6),
-            _ => Err("Unknown opcode".to_string()),
-        }
-    }
+opcodes! {
+    Stop(0),
+    Push0(95),
+    Push1(96),
+    Push2(97),
+    Push4(99),
+    Push6(101),
 }
 
 impl OpCode {
